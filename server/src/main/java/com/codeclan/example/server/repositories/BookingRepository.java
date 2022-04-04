38fa5945dc1794b96cf556d8f50cf000b121af33
tvpaterson <taylorpaterson7@gmail.com>
2022-04-04 13:41:29 +0100

    removed daves test comment

--- server/src/main/java/com/codeclan/example/server/repositories/BookingRepository.java
+++ server/src/main/java/com/codeclan/example/server/repositories/BookingRepository.java
@@ -4,9 +4,7 @@ import com.codeclan.example.server.models.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookingRepository extends JpaRepository <Booking, Long> {
-<<<<<<< HEAD
 //    Taylor's Comment
-=======
-//    David's comment
->>>>>>> 2eb5e393fab49a40d180cd1935fead78e406706a
+
+
 }
