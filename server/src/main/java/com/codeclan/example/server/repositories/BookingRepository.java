5c9f21fdc38c8ee14a71ae365a9f476af4044540
tvpaterson <taylorpaterson7@gmail.com>
2022-04-04 13:27:05 +0100

    testing commits

--- server/src/main/java/com/codeclan/example/server/repositories/BookingRepository.java
+++ server/src/main/java/com/codeclan/example/server/repositories/BookingRepository.java
@@ -4,4 +4,5 @@ import com.codeclan.example.server.models.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookingRepository extends JpaRepository <Booking, Long> {
+//    Taylor's Comment
 }
