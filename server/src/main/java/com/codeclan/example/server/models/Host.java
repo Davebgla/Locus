76ac859470aa5f973df321aabaec7fc794ed73f0
tvpaterson <taylorpaterson7@gmail.com>
2022-04-04 14:56:45 +0100

    added annotations to Host controller

--- server/src/main/java/com/codeclan/example/server/models/Host.java
+++ server/src/main/java/com/codeclan/example/server/models/Host.java
@@ -3,16 +3,27 @@ package com.codeclan.example.server.models;
 
 import com.fasterxml.jackson.databind.annotation.JsonAppend;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "hosts")
 public class Host extends User {
 
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+   @Column
     private List<Property> properties;
 
     public Host(String firstName, String lastName, String email, String contactNumber, int rating) {
         super(firstName, lastName, email, contactNumber, rating);
         this.properties = new ArrayList<>();
+    }
+
+    public Host(){
+
     }
 
     public List<Property> getProperties() {
