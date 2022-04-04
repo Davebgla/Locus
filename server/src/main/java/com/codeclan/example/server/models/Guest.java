e0345f064d0fa132235eed2be61ef81f73380b11
David Abercrombie <abercrombiedj2@gmail.com>
2022-04-04 13:22:14 +0100

    added guest model

--- server/src/main/java/com/codeclan/example/server/models/Guest.java
+++ server/src/main/java/com/codeclan/example/server/models/Guest.java
@@ -1,4 +1,21 @@
 package com.codeclan.example.server.models;
 
-public class Guest {
+import java.util.ArrayList;
+
+public class Guest extends User{
+
+    ArrayList<Booking> bookings;
+
+    public Guest(String firstName, String lastName, String email, String contactNumber, int rating) {
+        super(firstName, lastName, email, contactNumber, rating);
+        this.bookings = new ArrayList<>();
+    }
+
+    public ArrayList<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(ArrayList<Booking> bookings) {
+        this.bookings = bookings;
+    }
 }
