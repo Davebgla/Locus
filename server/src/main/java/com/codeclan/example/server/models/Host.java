6fac9120b34c2fc226ac436eb2f645c524bcbdd1
tvpaterson <taylorpaterson7@gmail.com>
2022-04-04 13:23:29 +0100

    added Host models

--- server/src/main/java/com/codeclan/example/server/models/Host.java
+++ server/src/main/java/com/codeclan/example/server/models/Host.java
@@ -1,4 +1,25 @@
 package com.codeclan.example.server.models;
 
-public class Host {
+
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Host extends User {
+
+    private List<Property> properties;
+
+    public Host(String firstName, String lastName, String email, String contactNumber, int rating) {
+        super(firstName, lastName, email, contactNumber, rating);
+        this.properties = new ArrayList<>();
+    }
+
+    public List<Property> getProperties() {
+        return properties;
+    }
+
+    public void setProperties(List<Property> properties) {
+        this.properties = properties;
+    }
 }
