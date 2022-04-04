1fac8dcc15c9f2bdd7d98a7ab4abacaf723c98bf
rory <rory-smith14@hotmail.co.uk>
2022-04-04 12:41:04 +0100

    property class

--- server/src/main/java/com/codeclan/example/server/models/Property.java
+++ server/src/main/java/com/codeclan/example/server/models/Property.java
@@ -1,4 +1,47 @@
 package com.codeclan.example.server.models;
 
 public class Property {
+
+    private long id;
+    private Host host;
+    private String address;
+    private int pricePerNight;
+
+    public Property(Host host, String address, int pricePerNight) {
+        this.host = host;
+        this.address = address;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Host getHost() {
+        return host;
+    }
+
+    public void setHost(Host host) {
+        this.host = host;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPrice() {
+        return pricePerNight;
+    }
+
+    public void setPrice(int price) {
+        this.pricePerNight = price;
+    }
 }
