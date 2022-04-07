fcf4453956622f1f2bd4cb44613faed713e06365
David Abercrombie <abercrombiedj2@gmail.com>
2022-04-07 11:41:05 +0100

    moved fetch up to app and passed state as props

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,29 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 import Featured from './components/Featured';
 import Events from './components/Events';
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
 
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    getProperties();
+  }, []);
+
+  const getProperties = async () => {
+
+    const api = await fetch('http://localhost:8080/api/properties');
+    const data = await api.json();
+
+    setProperties(data);
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
-      <Featured />
+      <Featured properties={properties} />
       <Events />
       </BrowserRouter>
     </div>
