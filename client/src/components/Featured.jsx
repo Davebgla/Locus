08a832cb94f617b267bccb399291d0fa043980f8
Davebgla <91500089+Davebgla@users.noreply.github.com>
2022-04-07 11:01:41 +0100

    first styling for featured properties

--- client/src/components/Featured.jsx
+++ client/src/components/Featured.jsx
@@ -18,7 +18,6 @@ function Featured() {
     const data = await api.json();
 
     setProperties(data);
-    console.log(data);
   }
 
   const featured = properties.splice(0, 10);
@@ -31,7 +30,7 @@ function Featured() {
                 perPage: 4,
                 pagination: false,
                 drag: "free",
-                gap: "5rem"
+                gap: "3rem"
             }}>
                 {featured.map((property) => {
                     return(
@@ -52,11 +51,49 @@ function Featured() {
 }
 
 const Wrapper = styled.div`
-    
+    margin: 4rem 0rem;
 `;
 
 const Card = styled.div`
-    
+    min-height: 15rem;
+    border-radius: 2rem;
+    overflow: hidden;
+    position: relative;
+
+    img{
+        border-radius: 2rem;
+        position: absolute;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    p{
+        position: absolute;
+        z-index: 10;
+        left: 50%;
+        bottom: 0%;
+        transform: translate(-50%, 0%);
+        color: white;
+        width: 100%;
+        text-align: center;
+        font-weight: 600;
+        font-size: 1rem;
+        height: 40%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+`;
+
+const Gradient = styled.div`
+        z-index: 3;
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.8));
+
 `;
 
 export default Featured;
