580bcefd578fabc0633c47e3618a7194883982c6
David Abercrombie <abercrombiedj2@gmail.com>
2022-04-07 15:24:22 +0100

    added property component

--- client/src/pages/Pages.jsx
+++ client/src/pages/Pages.jsx
@@ -1,16 +1,22 @@
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Home from './Home';
 import PropertyList from './Properties';
+import Property from './Property';
 
 function Pages({properties}) {
 
+
     const location = useLocation();
+
+
 
     return(
         <Routes location={location} key={location.pathname}>
             <Route path="/" element={<Home properties={properties} />} />
             <Route path="/properties" element={<PropertyList properties={properties} />} />
             {/* <Route path="/properties/:id" element={<Property />} /> */}
+            <Route exact path="/properties/:id" element={ <Property properties={properties}/>
+            }/>
         </Routes>
     )
 }
