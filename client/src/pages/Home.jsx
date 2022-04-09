26c010ecaf18cb2a28994bdd4a6bb7f51b011587
tvpaterson <taylorpaterson7@gmail.com>
2022-04-09 12:54:41 +0100

    added banner image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,21 @@
 import Featured from "../components/Featured";
 import Events from "../components/Events";
 import GuestForm from "../components/GuestForm";
+import { Card, CardMedia } from "@mui/material";
 
 
 function Home({properties, stateGuest, onCreate}) {
 
     return(
         <div className="home">
-            
+            <Card sx={{maxWidth: 1300, maxHeight: 700, margin: 0}}>
+                <CardMedia
+                component="img"
+                src="https://i.imgur.com/YtOrFVb.png"
+                alt="home-page-banner"
+                />
             <GuestForm guest={stateGuest} onCreate={onCreate}/>
+            </Card>
             <Featured properties={properties} />
             <Events />
         </div>
